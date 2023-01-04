a8db3cabd9c4ecc3f1597a130c014c006f781c1a
Ernest <99025962+EaleksS@users.noreply.github.com>
2023-01-05 00:19:49 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MY REACT ERNEST
         </a>
       </header>
     </div>
